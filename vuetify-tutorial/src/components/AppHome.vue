bb3e0a9257053e7cc140d828d4159077878b74f8
BriyanSitinjak <Imanbriyan@gmail.com>
2021-05-20 23:53:56 +0700

    add video on carousel

--- vuetify-tutorial/src/components/AppHome.vue
+++ vuetify-tutorial/src/components/AppHome.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app>
-    <v-carousel v-model="model">
-      <v-carousel-item v-for="(color, i) in colors" :key="color">
-        <v-sheet :color="color" height="100%" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="display-3">Slide {{ i + 1 }}</div>
-          </v-row>
-        </v-sheet>
-      </v-carousel-item>
+    <v-carousel>
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      ></v-carousel-item>
     </v-carousel>
     <v-card class="mx-auto" max-width="344">
       <v-img
@@ -60,10 +60,23 @@
 
 <script>
 export default {
-  data: () => ({
-    model: 0,
-    colors: ["primary", "indigo", "yellow darken-2", "brown", "orange"],
-    show: false,
-  }),
+  data() {
+    return {
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
+    };
+  },
 };
 </script>
