e6cc90036450486c52bf0fb4a007e9a3bd11045f
BriyanSitinjak <Imanbriyan@gmail.com>
2021-05-31 21:18:47 +0700

    tes import vue-class-component

--- vuetify-tutorial/src/data/HomeItem.js
+++ vuetify-tutorial/src/data/HomeItem.js
@@ -1,27 +1,27 @@
-class HomeItem {
-    // constructor(title, desc, icon) {
-    //     this.title = title;
-    //     this.desc = desc;
-    //     this.icon = icon;
-    // }
-
-    getData() {
-        let list = [];
-        list.push({
-            title:'loremipsumdollor amet',
-            desc:'loremipsumdollor amet',
-            icon:'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-        });
-        list.push({
-            title:'loremipsumdollor amet',
-            desc:'loremipsumdollor amet',
-            icon:'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-        });
-        list.push({
-            title:'loremipsumdollor amet',
-            desc:'loremipsumdollor amet',
-            icon:'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-        });
-        return list;
+export default class HomeItem {
+    constructor(title, desc, icon) {
+        this.title = title;
+        this.desc = desc;
+        this.icon = icon;
     }
+
+    // getData() {
+    //     let list = [];
+    //     list.push({
+    //         title:'loremipsumdollor amet',
+    //         desc:'loremipsumdollor amet',
+    //         icon:'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+    //     });
+    //     list.push({
+    //         title:'loremipsumdollor amet',
+    //         desc:'loremipsumdollor amet',
+    //         icon:'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+    //     });
+    //     list.push({
+    //         title:'loremipsumdollor amet',
+    //         desc:'loremipsumdollor amet',
+    //         icon:'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+    //     });
+    //     return list;
+    // }
 }
