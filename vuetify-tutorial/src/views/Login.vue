ded660d59829c9d72f8efd8c7356b57cd631ac0f
BriyanSitinjak <Imanbriyan@gmail.com>
2021-06-01 22:34:11 +0700

    login view

--- vuetify-tutorial/src/views/Login.vue
+++ vuetify-tutorial/src/views/Login.vue
@@ -1,5 +1,93 @@
 <template>
-  <!-- 30:16 Youtube -->
+  <v-app>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1
+                          class="text-center display-2 teal--text text--accent-3"
+                        >
+                          Sign In to Housing Payment
+                        </h1>
+                        <div class="text-center" mt-4>
+                          <v-btn class="mx-2" fab color="primary" outlined>
+                            <v-icon> fab fa-facebook</v-icon>
+                          </v-btn>
+                        </div>
+                      </v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2"> </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    step: 1,
+  }),
+};
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <template>
   <v-container>
     <form>
       <v-text-field
@@ -115,4 +203,4 @@ export default {
     },
   },
 };
-</script>
+</script> -->
