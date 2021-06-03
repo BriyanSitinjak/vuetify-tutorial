8f96a4ffb1a1b6211737cdf472b3c65111dd106a
BriyanSitinjak <Imanbriyan@gmail.com>
2021-06-03 22:37:39 +0700

    finshing view login

--- vuetify-tutorial/src/views/Login.vue
+++ vuetify-tutorial/src/views/Login.vue
@@ -11,22 +11,22 @@
                     <v-col cols="12" md="8">
                       <v-card-text class="mt-12">
                         <h1
-                          class="text-center display-2 teal--text text--accent-3"
+                          class="text-center display-2 primary--text text--accent-3"
                         >
-                          Sign In to Housing Payment
+                          Sign In Now
                         </h1>
-                        <div class="text-center" mt-4>
-                          <v-btn class="mx-2" fab color="primary" outlined>
-                            <v-icon> fab fa-facebook</v-icon>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon> fab fa-facebook-f</v-icon>
                           </v-btn>
-                          <v-btn class="mx-2" fab color="primary" outlined>
-                            <v-icon>fab fa-google-plus-g</v-icon>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google</v-icon>
                           </v-btn>
-                          <v-btn class="mx-2" fab color="primary" outlined>
+                          <v-btn class="mx-2" fab color="black" outlined>
                             <v-icon> fab fa-linkedin-in</v-icon>
                           </v-btn>
                         </div>
-                        <h4 class="text-center mlt-4">
+                        <h4 class="text-center mt-4">
                           Ensure your email for registration
                         </h4>
                         <v-form>
@@ -35,7 +35,7 @@
                             name="Email"
                             prepend-icon="email"
                             type="text"
-                            color="teal accent-3"
+                            color="primary accent-3"
                           />
                           <v-text-field
                             id="password"
@@ -43,28 +43,104 @@
                             name="Password"
                             prepend-icon="lock"
                             type="password"
-                            color="teal accent-3"
+                            color="primary accent-3"
                           />
                         </v-form>
+                        <h5 class="text-center mt-3">Forget Your Passsword?</h5>
                       </v-card-text>
+                      <div class="text-center mb-3">
+                        <v-btn rounded color="primary accent-3">
+                          SIGN IN
+                        </v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" md="4" class="primary accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1 mt-2">Hello, Ges</h1>
+                        <h3 class="text-center mt-4">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                        </h3>
+                      </v-card-text>
+                      <div class="text-center mt-2">
+                        <v-btn
+                          rounded
+                          outlined
+                          class="white primary--text"
+                          @click="step++"
+                          >SIGN UP</v-btn
+                        >
+                      </div>
                     </v-col>
                   </v-row>
                 </v-window-item>
                 <v-window-item :value="2">
-                  <v-row>
+                  <v-row class="fill-height">
+                    <v-col cols="12" md="4" class="primary accent-3">
+                      <v-card-text class="white--text accent-3 mt-14">
+                        <h1 class="text-center display-1">Welcome Back, Ges</h1>
+                        <h3 class="text-center mt-4">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit.
+                        </h3>
+                      </v-card-text>
+                      <div class="text-center mt-2">
+                        <v-btn
+                          class="white primary--text"
+                          rounded
+                          outlined
+                          @click="step--"
+                          >SIGN IN</v-btn
+                        >
+                      </div>
+                    </v-col>
                     <v-col cols="12" md="8">
                       <v-card-text class="mt-12">
                         <h1
-                          class="text-center display-2 teal--text text--accent-3"
+                          class="text-center display-2 primary--text text-accent-3"
                         >
-                          Register to Housing Payment
+                          Create Account
                         </h1>
-                        <div class="text-center" mt-4>
-                          <v-btn class="mx-2" fab color="primary" outlined>
-                            <v-icon> fab fa-twitter</v-icon>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon> fab fa-facebook-f</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
                           </v-btn>
                         </div>
+                        <h4 class="text-center mt-4">
+                          Ensure your email for registration
+                        </h4>
+                        <v-form>
+                          <v-text-field
+                            color="primary accent-3"
+                            label="Name"
+                            name="Name"
+                            prepend-icon="person"
+                            type="text"
+                          ></v-text-field>
+                          <v-text-field
+                            color="primary accent-3"
+                            label="Email"
+                            name="Email"
+                            prepend-icon="email"
+                            type="text"
+                          ></v-text-field>
+                          <v-text-field
+                            color="primary accent-3"
+                            label="Password"
+                            name="Password"
+                            prepend-icon="lock"
+                            type="password"
+                          ></v-text-field>
+                        </v-form>
                       </v-card-text>
+                      <div class="text-center mb-4">
+                        <v-btn color="primary accent-3" rounded>SIGN UP</v-btn>
+                      </div>
                     </v-col>
                   </v-row>
                 </v-window-item>
